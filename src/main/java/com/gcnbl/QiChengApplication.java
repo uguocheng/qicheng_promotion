e638c2b8b9e0a51dc94890e47511783aeccaeaa1
wuguocheng <wuguocheng@wgc.cn>
2025-12-15 23:10:32 +0800

    添加启动类

---
+++ src/main/java/com/gcnbl/QiChengApplication.java
@@ -0,0 +1,11 @@
+package com.gcnbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiChengApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QiChengApplication.class, args);
+    }
+}
